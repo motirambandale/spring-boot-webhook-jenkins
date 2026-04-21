9c821763fe6623419667fde236e57e5e90003fb4
motirambandale <bandalemotiram555@gmail.com>
2026-04-21 19:32:38 +0530

    Added Employee class

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+public class Employee {
+	
+	private Integer empId;
+	private String empName;
+	private Integer empSalary;
+	
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Integer getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(Integer empSalary) {
+		this.empSalary = empSalary;
+	}
+}
